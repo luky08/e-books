2e84676ef65d642924651937251b2ebcc8fecda9
Lukáš Helcl <luky.helcl@gmail.com>
2025-08-22 11:48:10 +0200

    feat: add navigation

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,53 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { BrowserRouter as Router, Routes, Route, useParams, useNavigate } from "react-router-dom";
+import "./App.css";
+import BookList from "./Books/BookList";
+import BookDetails from "./Books/BookDetails";
 
-function App() {
+const Books = [
+  {
+    id: 1,
+    title: "The Wim Hof Method: Activate Your Full Human Potential",
+    author: "Wim Hof",
+    year: 2020,
+    genre: "Self-help / Health",
+    priceCZK: 399,
+    rating: 4.3,
+    cover: "wim-hof.jpg",
+  },
+  {
+    id: 2,
+    title: "Steve Jobs",
+    author: "Walter Isaacson",
+    year: 2011,
+    genre: "Biography",
+    priceCZK: 349,
+    rating: 4.7,
+    cover: "steve-jobs.jpg",
+  },
+];
 
-  return (
-    <>
-    
-    </>
-  )
+function BookDetailsPage() {
+  const { id } = useParams();
+  const navigate = useNavigate();
+  const book = Books.find((b) => b.id === Number(id));
+
+  if (!book) {
+    return <p>Book not found.</p>;
+  }
+
+  return <BookDetails book={book} onBack={() => navigate("/")} />;
 }
 
-export default App
+function App() {
+  return (
+    <Router>
+      <h1>List of e-books</h1>
+      <Routes>
+        <Route path="" element={<BookList books={Books} />} />
+        <Route path="book/:id" element={<BookDetailsPage />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
