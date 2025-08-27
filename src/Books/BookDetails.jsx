aed55ab23e85dc3ba34a9b19e19c25b6ed5415dd
Lukáš Helcl <luky.helcl@gmail.com>
2025-08-27 10:26:36 +0200

    feat: add API to book detail that loads a book by ID from URL

--- src/Books/BookDetails.jsx
+++ src/Books/BookDetails.jsx
@@ -1,27 +1,51 @@
 import './BookDetails.css';
 import { useNavigate } from 'react-router-dom';
+import { apiGet } from "../utils/api";
+import { useParams } from 'react-router-dom';
+import { useState, useEffect } from 'react';
 
-export default function BookDetails({ book }) {
+export default function BookDetails() {
   const navigate = useNavigate();
 
-  if (!book) return null;
+
+    const { id } = useParams();
+    const [book, setBook] = useState({});
+
+
+    // When the `id` changes → call the API /api/books/:id
+    // Find the book that matches the current `id` in the URL
+    // Store that book in the `book` state
+    // The component re-renders with the book's details
+    useEffect(() => {
+        apiGet("/api/books/" + id)
+            .then((res) => {
+            if (Array.isArray(res?.data)) {
+              const found = res.data.find(b => String(b.book_id) === String(id));
+              setBook(found || null);
+            } else {
+              setBook(null);
+            }
+          })
+      }, [id]);
+
+      if (!book) { return "Book not found"}
 
   return (
     <div className="book-detail">
       <div className="book-content">
         <div className="book-cover">
-          <img
+          {/*<img
             src={book.cover ? `/assets/${book.cover}` : `/assets/placeholder.jpg`}
             alt={`Obálka knihy ${book.title}`}
-          />
+          />*/}
         </div>
 
         <div className="book-info">
           <h1 className="book-title">{book.title}</h1>
-          <p><strong>Author:</strong> {book.author}</p>
-          <p><strong>Genre:</strong> {book.genre || "—"}</p>
-          <p><strong>Year of publication:</strong> {book.year}</p>
-          <p><strong>Price:</strong> {book.priceCZK ? `${book.priceCZK} Kč` : "—"}</p>
+          <p><strong>Author:</strong> {book.author_name}</p>
+          <p><strong>Genre:</strong> {book.genre_name || "—"}</p>
+          <p><strong>Year of publication:</strong> {book.publication_year}</p>
+          <p><strong>Price:</strong> {book.price ? `${book.price} Kč` : "—"}</p>
           <p><strong>Rating:</strong> {book.rating ? `${book.rating}/5` : "—"}</p>
         </div>
       </div>
