3f7a35975dd948bf89cb766ac30bf07e985bdd76
Lukáš Helcl <luky.helcl@gmail.com>
2025-08-27 12:56:17 +0200

    delete: delete Button for login

--- src/Head.jsx
+++ src/Head.jsx
@@ -4,7 +4,7 @@ export default function() {
     return (
         <header className="head no-print">
             <h1>List of e-books</h1>
-            <button className="login-btn">Login</button>
+            {/*<button className="login-btn">Login</button>*/}
         </header>
     );
 }
